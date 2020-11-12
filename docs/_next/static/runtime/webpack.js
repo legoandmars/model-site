840dedafa676e0a720665f745b057498861ea78a
leogandmars <legoandmars@gmail.com>
2020-11-12 13:51:16 -0800

    chore: regen site

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "68c9c0773fa49bfeee7e";
+/******/ 	var hotCurrentHash = "5a3b869edb044de40ff3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
