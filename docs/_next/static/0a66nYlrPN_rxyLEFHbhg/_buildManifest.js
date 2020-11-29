93ad8eb7d0c0d7c2fe123838fdd6087a5c5e0787
leogandmars <legoandmars@gmail.com>
2020-11-28 20:00:36 -0800

    chore: regen site

---
+++ docs/_next/static/0a66nYlrPN_rxyLEFHbhg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/[id]":[a,"static\u002Fcss\u002Fe55e2cfce790c31fdfe7.css"],"/index":[a],"/notes":[a],"/packs":[a],"/walls":[a]}}("static\u002Fchunks\u002F9f7ac8d4c08348af728c6bc003afcb40c6613296.1d54f7180ad1bd6744ff.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- docs/_next/static/GsXg7m8j237DAaQlFjEbM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/[id]":[a,"static\u002Fcss\u002Fe55e2cfce790c31fdfe7.css"],"/index":[a],"/notes":[a],"/packs":[a],"/walls":[a]}}("static\u002Fchunks\u002F23320d9a98eb104f017baffaaf5b786904f4ce06.1d54f7180ad1bd6744ff.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
