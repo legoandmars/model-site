54567c50352542111ae514c2d0110996035a4e9f
leogandmars <legoandmars@gmail.com>
2020-11-30 21:04:13 -0800

    chore: regen site

--- docs/_next/static/drcE35jbZjm-rkspXjKTL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/[id]":[a,"static\u002Fcss\u002Fe55e2cfce790c31fdfe7.css"],"/index":[a],"/notes":[a],"/packs":[a],"/walls":[a]}}("static\u002Fchunks\u002Fab66b786eaca0f19c5fe96a996c5defc6e65bbd9.1d54f7180ad1bd6744ff.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
