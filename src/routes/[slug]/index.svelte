621a15fb223ef09f4826300d6479fa05483056f6
legoandmars <legoandmars@gmail.com>
2022-03-25 12:13:28 -0700

    no more adsense

--- src/routes/[slug]/index.svelte
+++ src/routes/[slug]/index.svelte
@@ -44,7 +44,43 @@
 </script>
 
 <Navbar />
-<div class="w-[100%] pt-40 sm:pt-32"></div>
+
+
+<div class="mx-auto w-[90%] 2xl:w-[75%] pt-32 pb-2 sm:pt-16 sm:pb-16" align="center">
+    {#if postData.endpoint.toLowerCase() != "fallguys" && postData.endpoint.toLowerCase() != "dripnotes" && postData.endpoint.toLowerCase() != "MinecraftWallPack"}
+        <!-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8003053308644369"
+        crossorigin="anonymous"></script> -->
+        <!-- Responsive ad unit -->
+        <!-- <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-8003053308644369"
+            data-ad-slot="3670690695"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script> -->
+        <div id="953660264">
+
+            <script type="text/javascript">
+        
+                try {
+        
+                    window._mNHandle.queue.push(function (){
+        
+                        window._mNDetails.loadTag("953660264", "970x250", "953660264");
+        
+                    });
+        
+                }
+        
+                catch (error) {}
+        
+            </script>
+        </div>
+    {/if}
+</div>
+
 <div class="mx-auto w-[85%] sm:w-[70%] max-h-lg flex flex-col items-start rounded-lg border shadow-md sm:flex-col border-2 border-stone-800 bg-black rounded-lg overflow-hidden">
     <div class="w-[100%] flex flex-col sm:flex-row items-start">
         <div class="w-[100%] sm:w-[50%] rounded-t-lg sm:rounded-l-lg">
@@ -102,22 +138,6 @@
     <div class="w-[100%] pt-8"></div>
     <FNFCredits />
 {/if}
-
-<div class="mx-auto w-[90%] 2xl:w-[75%] pt-16 pb-8 sm:pt-16 sm:pb-16" align="center">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8003053308644369"
-    crossorigin="anonymous"></script>
-    <!-- Responsive ad unit -->
-    <ins class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-8003053308644369"
-        data-ad-slot="3670690695"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-</div>
-
 
 <Footer />
 
